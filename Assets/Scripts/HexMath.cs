633b0890e5de02e837b1c5551abe0d6192fe2cdb
tory37 <tory37@gmail.com>
2015-09-30 10:46:48 -0500

    Added some functions to HexMath

--- Assets/Scripts/HexMath.cs
+++ Assets/Scripts/HexMath.cs
@@ -1,15 +1,119 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HexMath : MonoBehaviour {
+public class HexMath {
 
-	// Use this for initialization
-	void Start () {
-	
+	public enum EdgeDimension
+	{
+		EdgeWidth,
+		EdgeRadius
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public enum PointDimension
+	{
+		PointWidth,
+		PointRadius,
 	}
+
+	/// <summary>
+	/// Returns the distance from a corner to the center of a hexagon.
+	/// </summary>
+	/// <param name="dimension">The width from flat edge to flat edge or 
+	///		the radius from center to flat edge of the hexagon </param>
+	/// <param name="dimensionType">The enum to tell the function whether
+	///		/dimension/ is the flat edge to flat edge width or radius </param>
+	/// <returns></returns>
+	public static float PointToCenterRadius(float dimension, EdgeDimension dimensionType)
+	{
+		float edgeRadius = dimension;
+
+		switch ( dimensionType )
+		{
+			case EdgeDimension.EdgeWidth:
+				edgeRadius  = dimension * .5f;
+				break;
+			case EdgeDimension.EdgeRadius:
+				edgeRadius = dimension;
+				break;
+		}
+
+		return (2 * edgeRadius) / Mathf.Sqrt(3);
+	}
+
+	/// <summary>
+	/// Returns the distance between opposite corners of a hexagon.
+	/// </summary>
+	/// <param name="dimension">The width from flat edge to flat edge or 
+	///		the radius from center to flat edge of the hexagon</param>
+	/// <param name="dimensionType">The enum to tell the function whether
+	///		/dimension/ is the flat edge to flat edge width or radius </param>
+	/// <returns></returns>
+	public static float PointToPointWidth(float dimension, EdgeDimension dimensionType)
+	{
+		float edgeRadius = dimension;
+
+		switch ( dimensionType )
+		{
+			case EdgeDimension.EdgeWidth:
+				edgeRadius  = dimension * .5f;
+				break;
+			case EdgeDimension.EdgeRadius:
+				edgeRadius = dimension;
+				break;
+		}
+
+		return (4 * edgeRadius) / Mathf.Sqrt(3);
+	}
+
+	/// <summary>
+	/// Returns the distance from a flat edge to the center of a hexagon.
+	/// </summary>
+	/// <param name="dimension">The width from point to opposite point or 
+	///		the radius from center to point of the hexagon</param>
+	/// <param name="dimensionType">The enum to tell the function whether
+	///		/dimension/ is the point to opposite point width or radius </param>
+	/// <returns></returns>
+	public static float EdgeToCenterRadius(float dimension, PointDimension dimensionType)
+	{
+		float pointRadius = dimension;
+
+		switch ( dimensionType )
+		{
+			case PointDimension.PointWidth:
+				pointRadius  = dimension * .5f;
+				break;
+			case PointDimension.PointRadius:
+				pointRadius = dimension;
+				break;
+		}
+
+		return (pointRadius * Mathf.Sqrt( 3 )) / 2;
+	}
+
+	/// <summary>
+	/// Returns the distance from a flat edge to the opposite flat edge of a hexagon.
+	/// </summary>
+	/// <param name="dimension">The width from point to opposite point or 
+	///		the radius from center to point of the hexagon</param>
+	/// <param name="dimensionType">The enum to tell the function whether
+	///		/dimension/ is the point to opposite point width or radius </param>
+	/// <returns></returns>
+	public static float EdgeToEdgeWidth(float dimension, PointDimension dimensionType)
+	{
+		float pointRadius = dimension;
+
+		switch ( dimensionType )
+		{
+			case PointDimension.PointWidth:
+				pointRadius  = dimension * .5f;
+				break;
+			case PointDimension.PointRadius:
+				pointRadius = dimension;
+				break;
+		}
+
+		return 2 * ((pointRadius * Mathf.Sqrt( 3 )) / 2);
+	}
+
+	
 }
